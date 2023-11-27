88b0ead489e04206ad6a74c1208abbeac7a42d44
dmytro-voronin <d.voronin.owen@gmail.com>
2023-11-27 15:41:19 +0200

    add JpaRepository to BookRepository delete BookRepositoryImpl.java

--- src/main/java/mate/academy/bookstore/repository/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/BookRepository.java
@@ -3,11 +3,10 @@ package mate.academy.bookstore.repository;
 import java.util.List;
 import java.util.Optional;
 import mate.academy.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book save(Book book);
-
     Optional<Book> findById(Long id);
-
     List<Book> findAll();
 }
