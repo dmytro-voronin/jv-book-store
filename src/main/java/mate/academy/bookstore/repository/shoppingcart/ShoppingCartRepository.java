a13b47cb6905bd0dc189592f7c355448794d27b5
dmytro-voronin <d.voronin.owen@gmail.com>
2024-01-04 11:49:44 +0200

    delete empty line

--- src/main/java/mate/academy/bookstore/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/mate/academy/bookstore/repository/shoppingcart/ShoppingCartRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-
     @EntityGraph(attributePaths = "cartItems")
     ShoppingCart findByUserEmail(String email);
 }
